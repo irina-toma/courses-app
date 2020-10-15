357df5f2d8ccde2cb9963b59d8be7bc7f7a05a6f
Achitu24 <adrian.chitu7@gmail.com>
2020-10-15 15:55:34 +0300

    404page and login error page

--- controllers/messages/messages.js
+++ controllers/messages/messages.js
@@ -1,89 +1,97 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {
-    pool,
-    Message,
-    User
-} = require('../../models');
-const utils = require('../utils/utils.js');
-const {
-    authorizeAndExtractToken
-} = require('../security/jwt.js');
-const {
-    authorizeRoles
-} = require('../security/roles.js')
+const { pool, Message, User } = require("../../models");
+const utils = require("../utils/utils.js");
+const { authorizeAndExtractToken } = require("../security/jwt.js");
+const { authorizeRoles } = require("../security/roles.js");
 
 router.get("/", authorizeAndExtractToken, async (req, resp, next) => {
-    let currentId = req.state.decoded.userId;
+  let currentId = req.state.decoded.userId;
 
-    let messageList = await Message.findByOwnerSent(currentId);
+  let messageList = await Message.findByOwnerSent(currentId);
 
-    console.log(messageList);
-    
-    resp.render("messages", {
-        messageList
-    });
+  console.log(messageList);
 
+  resp.render("messages", {
+    messageList,
+  });
 });
 
 router.get("/sent", authorizeAndExtractToken, async (req, resp, next) => {
-    let currentId = req.state.decoded.userId;
+  let currentId = req.state.decoded.userId;
 
-    let messageList = await Message.findByOwnerSent(currentId);
-    
-    resp.render("messages", {
-        messageList
-    });
+  let messageList = await Message.findByOwnerSent(currentId);
 
+  resp.render("messages", {
+    messageList,
+  });
 });
 
 router.get("/received", authorizeAndExtractToken, async (req, resp, next) => {
-    let currentId = req.state.decoded.userId;
+  let currentId = req.state.decoded.userId;
 
-    let messageList = await Message.findByOwnerReceived(currentId);
-    console.log(messageList);
-    
-    resp.render("messages", {
-        messageList
-    });
+  let messageList = await Message.findByOwnerReceived(currentId);
+  console.log(messageList);
 
+  resp.render("messages", {
+    messageList,
+  });
 });
 
 router.post("/", authorizeAndExtractToken, async (req, resp, next) => {
-    // luam datele din request
-    let params = req.body;
-    // title, username, body
+  // luam datele din request
+  let params = req.body;
+  // title, username, body
 
-    if (!params.title || !params.to || !params.body) {
-        next("Missing parameters");
-    }
+  if (!params.title || !params.to || !params.body) {
+    next("Missing parameters");
+  }
 
-    let currentId = req.state.decoded.userId;
+  let currentId = req.state.decoded.userId;
 
-    // validam username
-    let user = await User.findByUsername(params.to);
-    if (user) {
-        let msgSender = new Message(user.id, currentId, currentId, params.body, params.title, null, null);
-        let resp = await msgSender.save();
+  // validam username
+  let user = await User.findByUsername(params.to);
+  if (user) {
+    let msgSender = new Message(
+      user.id,
+      currentId,
+      currentId,
+      params.body,
+      params.title,
+      null,
+      null
+    );
+    let resp = await msgSender.save();
 
-        let msgReceiver = new Message(user.id, currentId, user.id, params.body, params.title, null, null);
-        resp = await msgReceiver.save();
-    } else {
-        next("Wrong username");
-    }
+    let msgReceiver = new Message(
+      user.id,
+      currentId,
+      user.id,
+      params.body,
+      params.title,
+      null,
+      null
+    );
+    resp = await msgReceiver.save();
+  } else {
+    next("Wrong username");
+  }
 });
 
-router.post("/mailing-list", authorizeAndExtractToken, async (req, resp, next) => { // TODO: autorizat pt universitati si admin
+router.post(
+  "/mailing-list",
+  authorizeAndExtractToken,
+  async (req, resp, next) => {
+    // TODO: autorizat pt universitati si admin
     let params = req.body;
 
     if (!params.name || !params.usernameList) {
-        next("Missing parameters");
-    } 
-    else {
-        Message.addMailingList(params.name, params.usernameList);
-        resp.send(utils.success());
+      next("Missing parameters");
+    } else {
+      Message.addMailingList(params.name, params.usernameList);
+      resp.send(utils.success());
     }
-
-});
+  }
+);
 
 module.exports = router;
--- models/Message.js
+++ models/Message.js
@@ -1,85 +1,88 @@
 const pool = require("./pool.js");
 const utils = require("../controllers/utils/utils.js");
-const {
-    ServerError
-} = require("../controllers/utils/errors.js");
+const { ServerError } = require("../controllers/utils/errors.js");
 
 class Message {
-    constructor(to, from, owner, body, title, flags, toList) {
-        this.to = to;
-        this.from = from;
-        this.owner = owner;
-        this.body = body;
-        this.title = title;
-        this.flags = flags;
-        this.toList = toList;
+  constructor(to, from, owner, body, title, flags, toList) {
+    this.to = to;
+    this.from = from;
+    this.owner = owner;
+    this.body = body;
+    this.title = title;
+    this.flags = flags;
+    this.toList = toList;
+  }
+
+  static async findAll() {
+    const query = "SELECT * FROM messages";
+    return await Message.findByQuery(query);
+  }
+
+  static async findByQuery(query) {
+    let result = await pool.query(query);
+
+    return result.rows;
+  }
+
+  static async findByOwner(ownerId) {
+    const query = {
+      text: "SELECT * FROM messages WHERE owner=$1",
+      values: [ownerId],
+    };
+
+    return await Message.findByQuery(query);
+  }
+
+  static async findByOwnerSent(ownerId) {
+    const query = `select m.id, m.title, m.to, m.from, m.flags, m.body, m.to_list, u.username from messages as m, users as u where m.owner = ${ownerId} and m.from = ${ownerId} and m.to = u.id`;
+    return await Message.findByQuery(query);
+  }
+
+  static async findByOwnerReceived(ownerId) {
+    const query = `select m.id, m.title, m.to, m.from, m.flags, m.body, m.to_list, u.username from messages as m, users as u where m.owner = ${ownerId} and m.to = ${ownerId} and m.from = u.id`;
+    return await Message.findByQuery(query);
+  }
+
+  async save() {
+    const stm = {
+      text:
+        'INSERT INTO messages("to", "from", "owner", "body", "title", "flags", "to_list") VALUES($1, $2, $3, $4, $5, $6, $7)',
+      values: [
+        this.to,
+        this.from,
+        this.owner,
+        this.body,
+        this.title,
+        this.flags,
+        this.toList,
+      ],
+    };
+
+    let result = await pool.query(stm);
+
+    return result;
+  }
+
+  static async addMailingList(name, usernameList) {
+    // const stm = {
+    //     text: 'SELECT id from users WHERE username IN ($1)',
+    //     values: [usernameList]
+    // }
+
+    let names = usernameList.map((user) => `'${user}'`).join(",");
+    const stm = `SELECT id from users WHERE username IN (${names});`;
+
+    let result = await pool.query(stm);
+    let ids = result.rows;
+
+    if (ids.length > 0) {
+      for (let id of ids) {
+        const stm = `INSERT INTO mailing_list('name', 'user_id') VALUES (${name}, ${id})`; // TODO: fix this
+
+        await pool.query(stm);
+      }
     }
-
-    static async findAll() {
-        const query = 'SELECT * FROM messages';
-        return await Message.findByQuery(query);
-    }
-
-    static async findByQuery(query) {
-        let result = await pool.query(query);
-
-        return result.rows;
-    }
-
-    static async findByOwner(ownerId) {
-        const query = {
-            text: 'SELECT * FROM messages WHERE owner=$1',
-            values: [ownerId]
-        };
-
-        return await Message.findByQuery(query);
-    }
-
-    static async findByOwnerSent(ownerId) {
-        const query = `select m.id, m.title, m.to, m.from, m.flags, m.body, m.to_list, u.username from messages as m, users as u where m.owner = ${ownerId} and m.from = ${ownerId} and m.to = u.id`;
-        return await Message.findByQuery(query);
-    }
-
-    static async findByOwnerReceived(ownerId) {
-        const query = `select m.id, m.title, m.to, m.from, m.flags, m.body, m.to_list, u.username from messages as m, users as u where m.owner = ${ownerId} and m.to = ${ownerId} and m.from = u.id`;
-        return await Message.findByQuery(query);
-    }    
-
-    async save() {
-        const stm = {
-            text: 'INSERT INTO messages("to", "from", "owner", "body", "title", "flags", "to_list") VALUES($1, $2, $3, $4, $5, $6, $7)',
-            values: [this.to, this.from, this.owner, this.body, this.title, this.flags, this.toList],
-        };
-
-        let result = await pool.query(stm);
-
-        return result;
-    }
-
-    static async addMailingList(name, usernameList) {
-        // const stm = {
-        //     text: 'SELECT id from users WHERE username IN ($1)',
-        //     values: [usernameList]
-        // }
-
-       
-        let names = usernameList.map(user => `'${user}'`).join(',');
-        const stm = `SELECT id from users WHERE username IN (${names});`;
-
-        let result = await pool.query(stm);
-        let ids = result.rows;
-
-        if (ids.length > 0) {
-            for (let id of ids) {
-                const stm = {
-                    text: "INSERT INTO mailing_list('name', 'user_id') VALUES ('$1', $2)", // TODO: fix this
-                    values: [name, id]
-                }
-                await pool.query(stm);
-            }
-        }
-        
-    }
+  }
 }
 
 module.exports = Message;
